b03b45771240c41f8e95139583eca2188085d750
Ivan Krechetov <ikr@ikr.su>
2018-06-20 17:17:35 +0200

    Fetch and include the people data

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import People from 'People'
 
 ReactDOM.render(
-    (<h1>Well, hello!</h1>),
+    (<h1>Well, hello, {People[0].name.first}!</h1>),
     global.document.getElementById('root')
 )

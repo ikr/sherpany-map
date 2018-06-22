88e633722bc65e32cd83214fafee3785e38bfaa0
Ivan Krechetov <ikr@ikr.su>
2018-06-22 11:20:11 +0200

    Display the people markers on the map

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import People from 'People'
 import Map from './Map'
+import personMapMarker from './personMapMarker'
 
 ReactDOM.render(
-    (<Map/>),
+    (<Map markers={People.map(personMapMarker)}/>),
     global.document.getElementById('root')
 )

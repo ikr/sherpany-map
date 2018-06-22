76a333573315198aa436fd95fc0abc81f9778183
Ivan Krechetov <ikr@ikr.su>
2018-06-22 11:17:03 +0200

    Implement the personMapMarker()

---
+++ src/personMapMarker.js
@@ -0,0 +1,13 @@
+function capitalize (s) {
+    return s[0].toUpperCase() + s.substr(1)
+}
+
+export default function ({
+    name: {title, first, last},
+    location: {coordinates: {latitude, longitude}}
+}) {
+    return {
+        name: [title, first, last].map(capitalize).join(' '),
+        coordinates: [parseFloat(latitude), parseFloat(longitude)]
+    }
+}
---
+++ tests/personMapMarker.spec.js
@@ -0,0 +1,35 @@
+import assert from 'assert'
+import personMapMarker from '../src/personMapMarker'
+
+describe('personMapMarker', () => {
+    it('is a function', () => {
+        assert.strictEqual(typeof personMapMarker, 'function')
+    })
+
+    it('properly projects/transforms the person data', () => {
+        assert.deepEqual(
+            personMapMarker({
+                name: {title: 'monsieur', first: 'matthieu', last: 'roux'},
+                location: {
+                    street: '605 place du 22 novembre 1943',
+                    city: 'alle',
+                    state: 'vaud',
+                    postcode: 9424,
+                    coordinates: {latitude: '-50.9341', longitude: '-121.3184'},
+                    timezone: {
+                        offset: '+9:00',
+                        description: 'Tokyo, Seoul, Osaka, Sapporo, Yakutsk'
+                    }
+                },
+                email: 'matthieu.roux@example.com',
+                cell: '(826)-020-3024',
+                picture: {
+                    large: 'https://randomuser.me/api/portraits/men/81.jpg',
+                    medium: 'https://randomuser.me/api/portraits/med/men/81.jpg',
+                    thumbnail: 'https://randomuser.me/api/portraits/thumb/men/81.jpg'
+                }
+            }),
+            {name: 'Monsieur Matthieu Roux', coordinates: [-50.9341, -121.3184]}
+        )
+    })
+})

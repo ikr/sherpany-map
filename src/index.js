dffc8a9202755033323fe4dab81fe8cb3952544e
Ivan Krechetov <ikr@ikr.su>
2018-06-24 14:01:47 +0200

    Init the identifiedPeople

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import People from 'People'
+import identifyAll from './identifyAll'
 import Map from './Map'
 import personMapMarker from './personMapMarker'
 
+const identifiedPeople = identifyAll(1, People)
+
 ReactDOM.render(
-    (<Map markers={People.map(personMapMarker)}/>),
+    (<Map markers={identifiedPeople.map(personMapMarker)}/>),
     global.document.getElementById('root')
 )
